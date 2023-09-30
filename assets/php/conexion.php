ba865888b73823f62ea2b867fed9c43fb159d8b8
Coedu7 <119018528+Coedu7@users.noreply.github.com>
2023-09-30 18:21:42 -0400

    Update conexion.php

--- assets/php/conexion.php
+++ assets/php/conexion.php
@@ -1,8 +1,11 @@
 <?php
 
 define("host", 'localhost');
+//id21335425_eduardo
 define("user", 'root');
+//06092001Ce-
 define("pass", "");
+//id21335425_notas
 define("db", "notas");
 
 $conec = mysqli_connect(host, user, pass, db);
